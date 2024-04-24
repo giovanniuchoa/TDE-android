5bf966ac8ff5fb968ee16218f63c4371789b49c0
Giovanni Lucca Reque Uchoa <giovanni.uchoa@outlook.com>
2024-04-24 19:24:16 -0300

    asdasd

---
+++ app/src/main/java/com/example/teste/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.teste
+
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import com.example.teste.MainActivity2
+import com.example.teste.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var cameraController: LifecycleCameraController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        if (!hasPermissions(baseContext)){
+            activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+        }else{
+            startCamera()
+        }
+
+        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+    }
+
+    private fun startCamera(){
+        val previewView: PreviewView = viewBinding.viewFinder
+        cameraController = LifecycleCameraController(baseContext)
+        cameraController.bindToLifecycle(this)
+        cameraController.cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+        previewView.controller = cameraController
+    }
+
+    private fun takePhoto(){
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.ENGLISH).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        ).build()
+
+        cameraController.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = "Photo Capture Succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                    val intent = Intent(this@MainActivity, MainActivity2::class.java)
+                    startActivity(intent)
+
+                }
+            }
+        )
+    }
+
+
+
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+                permissions ->
+            var permissionGranted = true
+            permissions.entries.forEach{
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                    permissionGranted = true
+            }
+            if(!permissionGranted){
+                Toast.makeText(this,"Permission request denied",Toast.LENGTH_LONG).show()
+            }else{
+                startCamera()
+            }
+        }
+
+    companion object{
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "YYYY-MM-dd-HH-mm-ss-SSS"
+        private val REQUIRED_PERMISSIONS = mutableListOf(
+            android.Manifest.permission.CAMERA
+        ).apply {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P){
+                add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
+
+        fun hasPermissions(context: Context) = REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+}
